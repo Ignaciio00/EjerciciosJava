95297b63e6eae50358ff1b6e610ad7875782ae63
Ignacio <Ignacio.rom.97@gmail.com>
2021-12-17 19:17:34 +0100

    Hasta el ejercicio 5

--- src/ejerciciosjava/EjerciciosJava.java
+++ src/ejerciciosjava/EjerciciosJava.java
@@ -7,8 +7,12 @@ package ejerciciosjava;
  */
 public class EjerciciosJava {
 
-    public boolean fiestaArdillas(int numeroBellotas, boolean finDeSemana) {
-//Si es fi de semana da igual cuantas bellotas tengan 
+
+
+
+ public  boolean  fiestaArdillas ( int  numeroBellotas , boolean  finDeSemana ) {
+
+        //Si es fi de semana da igual cuantas bellotas tengan 
         if (finDeSemana) {
             return true;
         }
@@ -17,13 +21,75 @@ public class EjerciciosJava {
         }
         return false;
     }
+    /**
+@param velocidad
+@param birthday
+@param
+*/
+    public int multa(int velocidad, boolean birthday) {
+        if (birthday) {
+            velocidad -= 5;
+        }
+        if (velocidad <= 60) {//si vale a menos de 60, no hay multa 
+
+            return 0;
+        }
+        if (60 < velocidad && velocidad <= 80) {
+            return 1;//multa pequeña
+        }
+        return 2;
+    }
+public boolean  muyvanidoso(int multiplo ){
+if (multiplo %11==0){
+return true;
+
+
+}
+if(multiplo %11==1){
+return true;
+
+}
+return false;
+}
+public boolean SuenaTuMovil (boolean matinal,boolean madre,boolean dormido){
+
+if(matinal==false && madre==false &&dormido==false){
+return true;
+}
+if(matinal==false && madre==false &&dormido==true){
+return false ;
+}
+if(matinal==true && madre==false &&dormido==false){
+return false;
+}
+return false;
+}    
+
 
     public static void main(String[] args) {
+
+//test del primer ejercecio
         EjerciciosJava ejercicio = new EjerciciosJava();
-System.out.println("Ejercicio 01 :ARDILLAS");
-        System.out.println("30, false: "+ ejercicio.fiestaArdillas(30, false));
-        System.out.println("50, false: "+ejercicio.fiestaArdillas(50, false));
-        System.out.println("70, false: "+ejercicio.fiestaArdillas(70, true));
+        System.out.println("Ejercicio 01 :ARDILLAS");
+      System.out.println("30, false: " + ejercicio.fiestaArdillas(30, false));
+        System.out.println("50, false: " + ejercicio.fiestaArdillas(50, false));
+       System.out.println("70, false: " + ejercicio.fiestaArdillas(70, true));
+
+ //segundo ejercicio
+        System.out.println("Ejercicio 02 :multa");
+      System.out.println("60, false: " + ejercicio.multa(60, false));
+        System.out.println("65, false: " + ejercicio.multa(65, false));
+       System.out.println("65, true: " + ejercicio.multa(65, true));
+//tercer ejercicio
+ System.out.println("Ejercicio 03 :muyvanidoso");
+      System.out.println("22, true: " + ejercicio.muyvanidoso(22));
+        System.out.println("23, true: " + ejercicio.muyvanidoso(23));
+       System.out.println("24 false: " + ejercicio.muyvanidoso(24));
+
+ System.out.println("Ejercicio 04 :SuenaTuMovil");
+      System.out.println("false,false,false " + ejercicio.SuenaTuMovil(false, false, false));
+        System.out.println("false ,false,true: " +  ejercicio.SuenaTuMovil(false, false, true));
+       System.out.println(" true,false,false: " +  ejercicio.SuenaTuMovil(true, false, false));
     }
 
 }
